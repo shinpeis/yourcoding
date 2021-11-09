d74e96fc51ef2633e73fbead27939f6f0d7cbfa1
shinpeis <88133256+shinpeis@users.noreply.github.com>
2021-11-09 16:20:17 +0800

    Add files via upload

--- js/swiper.setting.js
+++ js/swiper.setting.js
@@ -1,2 +1,33 @@
-var mySwiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container', {
+  centeredSlides: true,
+  spaceBetween: 55,
+  breakpoints: {
+  // 768px以上の場合
+    320 :{
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    // 980px以上の場合
+    980: {
+      slidesPerView: 3.3,
+    },
+    // 1200px以上の場合
+    1200: {
+      slidesPerView: 3.5,
+    },
+    1800 :{
+      slidesPerView: 3,
+    }
+  },
+  loop: true,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  SimulateTouch: true
 });
